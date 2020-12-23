336bbdd9524a6e366452623e54e80ffe2dfed20b
lajawanti <lajawanti.dhake@gmail.com>
2020-12-23 15:06:43 -0500

    Basic setup and firebase configure

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Title.css';
+
+
+function Title() {
+      return (
+            <div className = "title">
+                 <h1>Photo Gallery</h1>
+                 <h2>Your Pictures</h2> 
+                 <p>.......................</p>
+            </div>
+      )
+}
+
+export default Title;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+
+var firebaseConfig = {
+      apiKey: process.env.REACT_APP_APIKEY,
+      authDomain: process.env.REACT_APP_AUTHDOMAIN,
+      projectId: process.env.REACT_APP_PROJECTID,
+      storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+      appId: process.env.REACT_APP_APPID,
+      measurementId: process.env.REACT_APP_MEASUREMENTID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
